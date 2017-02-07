45b51ae540985972830a605d744119cbd602d2b4
Ethan Jackson <jackson.ethan.c@gmail.com>
2017-02-07 12:51:54 -0500

    Added Course class and made changes to Student and Instructor.

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ethan on 2017-02-07.
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
+    }
+
+    public void addCourse(Course c){
+        if(this.courseList==null)
+            this.courseList = new ArrayList<>(5);
+        this.courseList.add(c);
     }
 
     public String toString() {
